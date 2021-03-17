9d6c6eab84eb9cce6c6f13ffede69608a73e0221
Jennifer Chow <jennego@gmail.com>
2021-03-16 23:09:24 -0700

    add grommet, in process of switching over

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-
-import './App.css';
-import Routes from './routes.js'
+import "./App.css";
+import Routes from "./routes.js";
 import NavBar from "./NavBar.js";
-import 'semantic-ui-css/semantic.min.css';
-
+import "semantic-ui-css/semantic.min.css";
+import { Grommet } from "grommet";
 
 class App extends Component {
   render() {
+    const theme = {
+      global: {
+        font: {
+          family: "Roboto",
+          size: "54px",
+          height: "20px",
+        },
+      },
+    };
+
     return (
-      <Router>
-      <div className="App">
-        <NavBar></NavBar>
-        <Routes></Routes>
-      </div>
-      </Router>
+      <Grommet theme={theme}>
+        <Router>
+          <div className="App">
+            <NavBar></NavBar>
+            <Routes></Routes>
+          </div>
+        </Router>
+      </Grommet>
     );
   }
 }
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Header, Button } from "semantic-ui-react";
+import { grommet, Button, Main } from "grommet";
 
 function WelcomePage(props) {
   return (
     <div className="WelcomePage">
-      <Container text>
-        <Header as="h2">Dressage Tests</Header>
+      <Main pad="large">
+        <h2>Dressage Tests</h2>
         <p>Simple directory of dressage tests.</p>
-        <Button primary as={Link} to="/tests">
-          {" "}
-          Go to Test Listing!
-        </Button>
-      </Container>
+
+        <Link to={"/tests"}>
+          <Button primary label="Go to Tests" />
+        </Link>
+      </Main>
     </div>
   );
 }
