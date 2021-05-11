f77c0fda2270cf760286994de2a823e67029c669
Jennifer Chow <jennego@gmail.com>
2021-05-10 17:32:34 -0700

    the access token needed to be configured with the audience

--- src/auth/auth-provider-with-history.js
+++ src/auth/auth-provider-with-history.js
@@ -5,6 +5,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   const history = useHistory();
 
@@ -18,6 +19,8 @@ const Auth0ProviderWithHistory = ({ children }) => {
       clientId={clientId}
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
+      audience={audience}
+      scope="view:all read:current_user update:current_user_metadata"
     >
       {children}
     </Auth0Provider>
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
+import ExternalApi from "../components/Auth/protected-api";
 
 const Profile = () => {
-  const { user } = useAuth0();
+  const {
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+    getAccessTokenWithPopup,
+  } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
   const { name, picture, email } = user;
+
+  // cannot be on localhost?
+  // useEffect(() => {
+  //   const getUserMetadata = async () => {
+  //     const domain = "dev-e6ihx-v9.us.auth0.com";
+
+  //     try {
+  //       const accessToken = await getAccessTokenWithPopup({
+  //         audience: `https://${domain}/api/v2/`,
+  //         scope: "read:current_user",
+  //       });
+
+  //       const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+  //       const metadataResponse = await fetch(userDetailsByIdUrl, {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         },
+  //       });
+
+  //       const { user_metadata } = await metadataResponse.json();
+
+  //       setUserMetadata(user_metadata);
+  //     } catch (e) {
+  //       console.log(e.message);
+  //     }
+  //   };
+
+  //   getUserMetadata();
+  // }, []);
+
+  // console.log(userMetadata);
 
   return (
     <div>
+      <ExternalApi />
+      {userMetadata}
       <div className="row align-items-center profile-header">
         <div className="col-md-2 mb-3">
           <img
@@ -25,6 +66,13 @@ const Profile = () => {
         <pre className="col-12 text-light bg-dark p-4">
           {JSON.stringify(user, null, 2)}
         </pre>
+
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )}
       </div>
     </div>
   );
